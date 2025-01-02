72b07a30aa64dc3fa79f229bf6af14c46a9b2013
Elias <72849929+Lumas13@users.noreply.github.com>
2025-01-02 21:04:10 +0800

    revert

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import MeetingArrivalPage from "./pages/MeetingArrivalPage";
-import MeetingConfigPage from "./pages/MeetingConfigPage";
-import MeetingRoomPage from "./pages/MeetingRoomPage";
-import MeetingPage from "./pages/MeetingPage";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/MeetingArrivalPage" element={<MeetingArrivalPage />} />
-        <Route path="/MeetingConfigPage" element={<MeetingConfigPage />} />
-        <Route path="/MeetingRoomPage" element={<MeetingRoomPage />} />
-        <Route path="/MeetingPage" element={<MeetingPage />} />
       </Routes>
     </Router>
   );
